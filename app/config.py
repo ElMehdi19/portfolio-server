03600fa1be25ca2412f2a52224719630fd628c27
El Mehdi Rami <lbiggboss19@gmail.com>
2020-09-24 00:06:32 +0100

    updated configuration files

--- app/config.py
+++ app/config.py
@@ -4,11 +4,10 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI = "sqlite:///portfolio.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_USERNAME = 'lbiggboss19@gmail.com'
-    MAIL_PASSWORD = 'coadqjvxcejxpiad'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_PORT = 465
     MAIL_USE_SSL = True
