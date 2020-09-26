22c0e9f015175555d7b187d5023e87b1efae4563
El Mehdi Rami <lbiggboss19@gmail.com>
2020-09-26 22:45:17 +0100

    updated redis_url default value to match localhost

--- app/worker.py
+++ app/worker.py
@@ -5,8 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv(
-    'REDISTOGO_URL', 'redis://redistogo:3dd68954d06aca596e12a0b8a863a5ae@pike.redistogo.com:10842/')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
